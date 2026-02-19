866224a319f09c8406ffb1fdd1f23126b09701c2
Fgshop <itsddan@gmail.com>
2026-02-20 01:06:25 +0900

    Edit api link

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+# api
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://jojuwallet-api.vercel.app/api/v1',
   headers: { 'Content-Type': 'application/json' },
